bff1844667b918893d08fa059abdb2861c204ac0
bmergl <bmergl@outlook.de>
2024-07-22 00:59:14 +0200

    WIP

--- examples/Z_analysis_zsplit.jl
+++ examples/Z_analysis_zsplit.jl
@@ -18,24 +18,25 @@ print("tehrahedrons: ", length(md.Ω.faces))
 #Visu.mesh(md.Ω) 
 
 #Quadstrat
-qs3D = BEAST.SingleNumQStrat(3)
-qs4D = BEAST.DoubleNumWiltonSauterQStrat(3,3,3,3,4,4,4,4) #BEAST.DoubleNumWiltonSauterQStrat(2,3,2,3,4,4,4,4)
-qs5D6D = BEAST.SauterSchwab3DQStrat(3,3,4,4,4,4)
-# qs3D = BEAST.SingleNumQStrat(6)
-# qs4D = BEAST.DoubleNumWiltonSauterQStrat(5,5,5,5,6,6,6,6) #BEAST.DoubleNumWiltonSauterQStrat(2,3,2,3,4,4,4,4)
-# qs5D6D = BEAST.SauterSchwab3DQStrat(5,5,6,6,6,6)
+# qs3D = BEAST.SingleNumQStrat(3)
+# qs4D = BEAST.DoubleNumWiltonSauterQStrat(3,3,3,3,4,4,4,4) #BEAST.DoubleNumWiltonSauterQStrat(2,3,2,3,4,4,4,4)
+# qs5D6D = BEAST.SauterSchwab3DQStrat(3,3,4,4,4,4)
+qs3D = BEAST.SingleNumQStrat(6)
+qs4D = BEAST.DoubleNumWiltonSauterQStrat(5,5,5,5,6,6,6,6) #BEAST.DoubleNumWiltonSauterQStrat(2,3,2,3,4,4,4,4)
+qs5D6D = BEAST.SauterSchwab3DQStrat(5,5,6,6,6,6)
 BEAST.defaultquadstrat(op::BEAST.LocalOperator, tfs, bfs) = qs3D
 BEAST.defaultquadstrat(op::BEAST.Helmholtz3DOp, tfs, bfs) = qs4D
 BEAST.defaultquadstrat(op::BEAST.VIEOperator, tfs, bfs) = qs5D6D
 
 
-f_list = [1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0, 1000000.0, 10000000.0 ] # in Hz
+f_list = [1.0, 10.0, 100.0, 1000.0, 10000.0, 100000.0, 1000000.0, 10000000.0] # in Hz
 Z_list = []
 Z2_list = []
+Z_ana_list = []
 for f in f_list
     sol, S, R = IP.solve(;
         md = md, 
-        material = IP.constantmaterial(0.2, 10000.0*IP.ε0),
+        material = IP.constant_zsplit(0.5, 3.0*IP.ε0, 0.0008, 0.2, 10.0*IP.ε0),
         κ0 = 0.1, # möglichst in der nähe der realen Größen wählen damit cond(S) klein?
         ϵ0 = 1.0*IP.ε0, # Größenordnung??? was ist sinnvoll?
         ω = 2*pi*f, 
@@ -53,42 +54,25 @@ for f in f_list
     U = sol.potential_top - sol.potential_bottom
     Z = U/I_top
     Z2 = U/I_top2
+    Z_ana = U/IP.solution_I_ana(md.body, sol.material, md, sol)
     push!(Z_list, Z)
     push!(Z2_list, Z2) 
+    push!(Z_ana_list, Z_ana)
 end
 ##
 
 ##
 
 
-Z_ana = []
 
-L_z = md.body.L_z
-L_x = md.body.L_x
-L_y = md.body.L_y   
-ϵ = sol.material.ϵ
-κ = sol.material.κ
-U = sol.potential_top-sol.potential_bottom
-R = (1/κ)*L_z/(L_x*L_y)
-C = ϵ*(L_x*L_y)/L_z
-
-for f in f_list
-    ω = 2*pi*f
-    Z_ = (R - im*ω*R^2*C)/(1 + ω^2*R^2*C^2)
-    push!(Z_ana,Z_)
-end
-
-
-
-
-## Plot
+## Plot: Real - Imag
 
 plt = Plots.plot(f_list, real.(Z_list), line=:line , label = "Re{Z}", size=(700,600), xaxis=:log10)
 plot!(plt, f_list, imag.(Z_list), label = "Im{Z}")
 plot!(plt, f_list, real.(Z2_list), label = "Re{Z2}")
 plot!(plt, f_list, imag.(Z2_list), label = "Im{Z2}")
-plot!(plt, f_list, real.(Z_ana), label = "Im{Z_ana}")
-plot!(plt, f_list, imag.(Z_ana), label = "Im{Z_ana}")
+plot!(plt, f_list, real.(Z_ana_list), label = "Re{Z_ana}")
+plot!(plt, f_list, imag.(Z_ana_list), label = "Im{Z_ana}")
 #xlims!(0.0, 1.0)
 #ylims!(-0.01, 0.0)
 #title!("J_z(x, y0, z0)")
@@ -96,9 +80,23 @@ xlabel!("f in Hz")
 ylabel!("Impedance in Ω")
 
 
+## Plot: Ab
+
+plt = Plots.plot(f_list, abs.(Z_list), line=:line , label = "|Z|", size=(700,600), xaxis=:log10)
+plot!(plt, f_list, abs.(Z2_list), label = "|Z2|")
+plot!(plt, f_list, abs.(Z_ana_list), label = "|Z_ana|")
+
+#xlims!(0.0, 1.0)
+ylims!(0.0, 600.0)
+#title!("J_z(x, y0, z0)")
+xlabel!("f in Hz")
+ylabel!("Impedance in Ω")
+
 ##
 
-
-
-
+plt = Plots.plot(f_list, angle.(Z_list)/(2*pi)*360, label = "arg{Z}", line=:line , size=(700,600), xaxis=:log10)
+plot!(plt, f_list, angle.(Z2_list)/(2*pi)*360, label = "arg{Z2}")
+plot!(plt, f_list, angle.(Z_ana_list)/(2*pi)*360, label = "arg{Z_ana}")
+xlabel!("f in Hz")
+ylabel!("Angle in °")
 
